56da60dc6b0fe021a53eb367ac09838dd6cab404
flowerburied <2842511561@qq.com>
2022-02-23 10:28:29 +0800

    导入axios 重写了index 把function改成class

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,27 +1,78 @@
 import React from "react";
 import { Swiper, Toast } from "antd-mobile";
 import "./index.css";
-export default function index() {
-  const colors = ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"];
-  const items = colors.map((color, index) => (
-    <Swiper.Item key={index}>
-      <div
-        className="content"
-        style={{ background: color }}
-        onClick={() => {
-          Toast.show(`你点击了卡片 ${index + 1}`);
-        }}
-      >
-        {index + 1}
-      </div>
-    </Swiper.Item>
-  ));
+// 导入axios
+import axios from "axios";
+export default class Index extends React.Component {
+  state = {
+    swipers: [],
+    colors: ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"],
+  };
 
-  return (
-    <div>
-      <Swiper autoplay loop>
-        {items}
-      </Swiper>
-    </div>
-  );
+  async getSwipers() {
+    const res = await axios.get("http://localhost:8080/home/swiper");
+    console.log("res", res);
+    // this.setState(()=>{
+    //   return{
+    //     swipers:res.data.body
+    //   }
+    // })
+  }
+
+  componentDidMount() {
+    this.getSwipers()
+  }
+
+  render() {
+    const items = this.state.colors.map((color, index) => (
+      <Swiper.Item key={index}>
+        <div
+          className="content"
+          style={{ background: color }}
+          onClick={() => {
+            Toast.show(`你点击了卡片 ${index + 1}`);
+          }}
+        >
+          {index + 1}{" "}
+        </div>{" "}
+      </Swiper.Item>
+    ));
+
+    return (
+      <div>
+        <Swiper autoplay loop>
+          {items}
+        </Swiper>
+      </div>
+    );
+  }
 }
+
+// export default function index() {
+//   const getSwipers = async () => {
+//     const res = await axios.get("http://localhost:8080/home/swiper");
+//     console.log("res", res);
+//   };
+//   const colors = ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"];
+//   const items = colors.map((color, index) => (
+//     <Swiper.Item key={index}>
+//       <div
+//         className="content"
+//         style={{ background: color }}
+//         onClick={() => {
+//           Toast.show(`你点击了卡片 ${index + 1}`);
+//         }}
+//       >
+//         {index + 1}{" "}
+//       </div>{" "}
+//     </Swiper.Item>
+//   ));
+//   return (
+//     <div>
+//       <Swiper autoplay loop>
+//         {" "}
+//         {items}{" "}
+//       </Swiper>{" "}
+//     </div>
+//   );
+// }
