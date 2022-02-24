196c68d5a743931fa00cf8aab328e57da4b8e3f9
flowerburied <2842511561@qq.com>
2022-02-24 09:22:26 +0800

    路由懒加载

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 // 导入路由
-import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { useRoutes } from "react-router-dom";
 
 // 导入首页和城市选择两个组件
-import CityList from "./pages/CityList";
-import Home from "./pages/Home";
-import Index from "./pages/Index";
-import HouseList from "./pages/HouseList";
+
 // 导入要使用的组件
 // import { Button } from 'antd-mobile'
 // import router from "./router/indexRouter";
+import router from "./router/indexRouter";
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<Index />}></Route>
-        <Route path="/houselist" element={<HouseList />}></Route>
-      </Route>
-      <Route path="/cityList" element={<CityList />}></Route>
-    </Routes>
-  );
+  return <Suspense fallback={<>loading</>}>{useRoutes(router)}</Suspense>;
+
+  // return useRoutes(router)
+  // const element = useRoutes(router);
+  // return (
+  //   <Routes>
+  //     <Route path="/" element={<Home />}>
+  //       <Route index element={<Index />}></Route>
+  //       <Route path="/houselist" element={<HouseList />}></Route>
+  //     </Route>
+  //     <Route path="/cityList" element={<CityList />}></Route>
+  //   </Routes>
+  // );
 }
 
 export default App;
--- src/router/indexRouter.js
+++ src/router/indexRouter.js
@@ -1,31 +1,45 @@
 // import React from "react";
-import * as React from "react";
-import { useRoutes } from "react-router-dom";
+// import * as React from "react";
+import { lazy } from "react";
+import { RouteObject } from "react-router-dom";
 // 导入首页和城市选择两个组件
 // import Home from "../pages/Home";
-import Home from "../pages/Home";
-import CityList from "../pages/CityList";
-import News from "../pages/News";
-import Index from "../pages/Index";
+// import Home from "../pages/Home";
+const Home = lazy(() => import("../pages/Home"));  //路由懒加载
+// import CityList from "../pages/CityList"
+const CityList = lazy(() => import("../pages/CityList"));
+// import News from "../pages/News"
+const News = lazy(() => import("../pages/News"));
+// import Index from "../pages/Index"
+const Index = lazy(() => import("../pages/Index"));
+// import HouseList from "../pages/HouseList"
+const HouseList = lazy(() => import("../pages/HouseList"));
+// import Profile from "../pages/Profile"
+const Profile = lazy(() => import("../pages/Profile"));
+const router: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "news",
+        element: <News />,
+      },
+      {
+        path: "houseList",
+        element: <HouseList />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  { path: "cityList", element: <CityList /> },
+];
+export default router;
 
-export default function App() {
-  let element = useRoutes([
-    {
-      path: "/",
-      element: <Home />,
-      children: [
-        { index: true, element: <Index /> },
-        {
-          path: "news",
-          element: <News />,
-        },
-      ],
-    },
-    { path: "cityList", element: <CityList /> },
-  ]);
-
-  return element;
-}
 // export default function indexRouter() {
 //   return (
 //     <BrowserRouter>
